7bc130e0c7c9459ee6d26e264ef15fad0d9c7520
iklam <iklam@openjdk.org>
2026-06-18 09:01:00 -0700

    @coleenp comment: use ASM to generate the test case

--- test/hotspot/jtreg/runtime/classFileParserBug/StackMapTooLong.java
+++ test/hotspot/jtreg/runtime/classFileParserBug/StackMapTooLong.java
@@ -24,105 +24,65 @@
 /*
  * @test Very large StackMapTable should cause OutOfMemoryError and not VM crash.
  * @bug 8386562
- * @library /test/lib
+ * @library /test/lib /test/hotspot/jtreg/testlibrary/asm
  * @run main StackMapTooLong
  */
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import jdk.test.lib.helpers.ClassFileAssembler;
+import java.lang.invoke.MethodHandles;
+import static org.objectweb.asm.Opcodes.*;
+import org.objectweb.asm.*;
 
 public class StackMapTooLong {
-    public static void main(String args[]) throws Throwable {
-        String jcodFile = "BadClass.jcod";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(jcodFile))) {
-            writer.write(head);
-            int max = 2100000;
-            for (int i = 0; i < max; i++) {
-                writer.write("0x0000000000000000;\n");
-            }
-            writer.write(tail);
-        }
+    private static String BadClass = "BadClass";
 
-        // Generate a StackMapTable that's larger than Metaspace::max_allocation_word_size()
-        ClassFileAssembler.compileJcod(jcodFile);
+    public static void main(String[] args) throws Exception {
+        byte[] classFile = dumpBadClass();
 
-        URL url = new File(System.getProperty("user.dir")).toURI().toURL();
-        URL[] urls = new URL[] {url};
-        URLClassLoader loader = new URLClassLoader(urls);
         try {
-            loader.loadClass("BadClass");
+            MethodHandles.lookup().defineClass(classFile);
             throw new RuntimeException("OutOfMemoryError expected but not thrown!");
         } catch (OutOfMemoryError expected) {}
     }
 
-    static final String head = """
-// This file is auto-generated
-class BadClass {
-  0xCAFEBABE;
-  0; // minor version
-  52; // version
-  [] { // Constant Pool
-    ; // first element is empty
-    Utf8 "BadClass"; // #1
-    class #1; // #2
-    Utf8 "java/lang/Object"; // #3
-    class #3; // #4
-    Utf8 "<init>"; // #5
-    Utf8 "()V"; // #6
-    NameAndType #5 #6; // #7
-    Method #4 #7; // #8
-    Utf8 "java/lang/Throwable"; // #9
-    class #9; // #10
-    Utf8 "Code"; // #11
-    Utf8 "StackMapTable"; // #12
-  } // Constant Pool
+    static class LargeStackMapTable extends Attribute {
+      LargeStackMapTable() {
+          super("StackMapTable");
+      }
 
-  0x0001; // access
-  #2;// this_cpx
-  #4;// super_cpx
+      @Override
+      public boolean isCodeAttribute() {
+          return true;
+      }
 
-  [] { // Interfaces
-  } // Interfaces
-
-  [] { // Fields
-  } // Fields
-
-  [] { // Methods
-    {  // method
-      0x0001; // access
-      #5; // name_index
-      #6; // descriptor_index
-      [] { // Attributes
-        Attr(#11) { // Code
-          1; // max_stack
-          1; // max_locals
-          Bytes[]{
-            0xB70008B100000000;
-          }
-          [] { // Traps
-          } // end Traps
-          [] { // Attributes
-            Attr(#12) { // StackMapTable
-              [] { // -- to be followed by *a lot* of zeros
-""";
-
-    static final String tail = """
-              }
-            } // end StackMapTable
-          } // Attributes
-        } // end Code
-      } // Attributes
+      @Override
+      protected ByteVector write(ClassWriter cw, byte[] code,
+                                 int codeLength, int maxStack, int maxLocals) {
+          int len = 16 * 1024 * 1024 + 1; // Too large to be allocated by Metaspace::allocate()
+          ByteVector bv = new ByteVector();
+          bv.putByteArray(new byte[len], 0, len);
+          return bv;
+      }
     }
-  } // Methods
 
-  [] { // Attributes
-  } // Attributes
-} // end class BadClass
-""";
+    private static byte[] dumpBadClass() throws Exception {
+      ClassWriter classWriter = new ClassWriter(0);
+      MethodVisitor methodVisitor;
 
+      classWriter.visit(51, ACC_SUPER, BadClass, null, "java/lang/Object",
+                        null);
+
+      {
+        methodVisitor =
+            classWriter.visitMethod(ACC_PUBLIC | ACC_STATIC, "main",
+                                    "([Ljava/lang/String;)V", null, null);
+        methodVisitor.visitCode();
+        methodVisitor.visitInsn(RETURN);
+        methodVisitor.visitAttribute(new LargeStackMapTable());
+        methodVisitor.visitMaxs(0, 1);
+        methodVisitor.visitEnd();
+      }
+      classWriter.visitEnd();
+
+      return classWriter.toByteArray();
+    }
 }
